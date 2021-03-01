50ca7c33d8f1eb5846ae3b9c2ec49612c2bfe477
come2america <ysserunjogi@gmail.com>
2021-02-28 21:51:22 -0800

    database

--- messanger/script2.js
+++ messanger/script2.js
@@ -119,6 +119,24 @@ messageInputDOM.addEventListener("keydown", (event) => {
       soundInputDOM.value = "";
   }
 });
+var database = firebase.database();
+messageInputDOM.addEventListener("keydown", (event) =>  {
+    event.preventDefault();
 
+    street = $("#street").val().trim().toLowerCase();
+    city = $("#city").val().trim().toLowerCase();
+    state = $("#state").val().trim().toLowerCase();
+
+    database.ref().push({
+        userstreet: street,
+        usercity: city,
+        userstate: state,
+       message:messageInputDOM,
+      sound: soundInputDOM,
+        name:namePickerDOM,
+
+    })
+
+});
 
 /* Setup Bitmoji Kit Web here */
