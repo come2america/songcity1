247af021499dcd161edcb2b68c03a1204a1c0cda
come2america <ysserunjogi@gmail.com>
2021-03-01 09:41:08 -0800

    games divs

--- messanger/script2.js
+++ messanger/script2.js
@@ -1,3 +1,10 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.9/firebase-messaging.js');
+
+
 /* Initialize Firebase */
 firebase.initializeApp({
   /* Fill in the following values based on your config. */
@@ -119,7 +126,43 @@ messageInputDOM.addEventListener("keydown", (event) => {
       soundInputDOM.value = "";
   }
 });
+const messaging = firebase.messaging();
+// Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+messaging.getToken({ vapidKey: "BEa6JegOxPWXtaslwY73PHf8OWJaQ7UBLgyqs32jB3K9ccMud4_OO53zNQ6fl77_w0WIrVD9jFbqyNKTN6tBnII" }).then((currentToken) => {
+    if (currentToken) {
+        // Send the token to your server and update the UI if necessary
+        // ...
+    } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+    }
+}).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+});
+messaging.onMessage((payload) => {
+    console.log('Message received. ', payload);
+    // ...
+});
+function onBackgroundMessage() {
+    const messaging = firebase.messaging();
 
+    // [START messaging_on_background_message]
+    messaging.onBackgroundMessage((payload) => {
+        console.log('[firebase-messaging-sw.js] Received background message ', payload);
+        // Customize notification here
+        const notificationTitle = 'Background Message Title';
+        const notificationOptions = {
+            body: 'Background Message body.',
+            icon: '/firebase-logo.png'
+        };
+
+        self.registration.showNotification(notificationTitle,
+            notificationOptions);
+    });
+  // [END messaging_on_background_message]
 
 
 
